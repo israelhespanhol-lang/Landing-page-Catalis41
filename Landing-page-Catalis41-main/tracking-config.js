8166b0ec3b8b5aa97d5b20307f3fafb1c810e167
israelhespanhol <israelhespanhol-lang@users.noreply.github.com>
2026-09-14 12:22:30 -0300

    Initial commit

---
+++ Landing-page-Catalis41-main/tracking-config.js
@@ -0,0 +1,10 @@
+/*
+ * Central de rastreamento da landing page Catalisa41.
+ * Preencha somente os identificadores que deseja ativar e publique novamente.
+ * O Google Tag Manager permite instalar tags de qualquer mídia sem alterar o site.
+ */
+window.CATALISA_TRACKING = {
+  googleTagManagerId: "", // Ex.: GTM-XXXXXXX
+  googleAnalyticsId: "",  // Ex.: G-XXXXXXXXXX
+  metaPixelId: "",        // Ex.: 123456789012345
+};
